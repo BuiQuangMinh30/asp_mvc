6581bd673bb7943052179bdbdab805e8875e0957
bqminh <64219602+BuiQuangMinh30@users.noreply.github.com>
2022-03-02 23:37:21 +0700

    asp

---
+++ MenualNetMVC/Data/SchoolContext .cs
@@ -0,0 +1,18 @@
+﻿using MenualNetMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MenualNetMVC.Data
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    
+    }
+}
---
+++ MenualNetMVC/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MenualNetMVC.Models
+{
+    public class Student
+    {
+        [DisplayName("Mã Số")]
+        public int ID { get; set; }
+
+        [DisplayName("Họ")]
+        public string LastName { get; set; }
+
+        [DisplayName("Tên")]
+        public string FirstMidName { get; set; }
+
+        [DisplayName("Ngày nhập học")]
+        public DateTime EnrollmentDate { get; set; }
+
+    }
+}
